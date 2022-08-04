77cc4d5e58200fe95b249843ec68523273820714
kwartal <bv.barinov@gmail.com>
2022-08-05 00:30:09 +0300

    Create main

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RGBSlider: UIViewController {
 
+    @IBOutlet weak var colorWindowView: UIView!
+    
+    @IBOutlet weak var nameRedColorLabel: UILabel!
+    @IBOutlet weak var nameGreenColorLabel: UILabel!
+    @IBOutlet weak var nameBlueColorLabel: UILabel!
+    
+    @IBOutlet weak var valueRedColorLabel: UILabel!
+    @IBOutlet weak var valueGreenColorLabel: UILabel!
+    @IBOutlet weak var valueBlueColorLabel: UILabel!
+    
+    @IBOutlet weak var RedColorSlider: UISlider!
+    @IBOutlet weak var GreenColorSlider: UISlider!
+    @IBOutlet weak var BlueColorSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
 
 }
 
