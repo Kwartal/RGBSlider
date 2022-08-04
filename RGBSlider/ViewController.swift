6e8167c45d22ac55bef173ae3e7a50f9ddcd2687
kwartal <bv.barinov@gmail.com>
2022-08-05 00:37:35 +0300

    add Outlets

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -9,7 +9,20 @@ import UIKit
 
 class RGBSlider: UIViewController {
 
-
+    @IBOutlet weak var colorWindowView: UIView!
+    
+    
+    @IBOutlet weak var nameRedColorLabel: UILabel!
+    @IBOutlet weak var nameGreenColorLabel: UIView!
+    @IBOutlet weak var nameBlueColorLabel: UILabel!
+    
+    @IBOutlet weak var valueRedColorLabel: UILabel!
+    @IBOutlet weak var valueGreenColorLabel: UILabel!
+    @IBOutlet weak var valueBlueColorLabel: UILabel!
+    
+    @IBOutlet weak var RedColorSlider: UISlider!
+    @IBOutlet weak var GreenColorSlider: UISlider!
+    @IBOutlet weak var BlueColorSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
